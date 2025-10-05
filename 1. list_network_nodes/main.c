87eb5853bb7600d57e3bfbce9e6e25e7bf7a6a72
Gaymocoder <gaymocoder@gmail.com>
2025-10-05 21:18:49 +0800

    B&W | list_network_nodes | Divided into functions
    — Added functions "print_ifaddr_info" and "print_inet_interfaces";
    — Removed its realisation from "main"-function;
    
    — Builds and works properly.

--- 1. list_network_nodes/main.c
+++ 1. list_network_nodes/main.c
@@ -5,6 +5,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_ifaddr_info(struct ifaddrs* address, uint8_t index)
+{
+    int addr_family = address->ifa_addr->sa_family;
+    
+    printf("Interface #%u:\n", index);
+    printf("    family:\t%s\n", (addr_family == AF_INET) ? "IPv4" : "IPv6");
+    printf("    name:\t%s\n", address->ifa_name);
+        
+    char text_address[100];
+    const int family_struct_size = (addr_family == AF_INET) ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
+    
+    getnameinfo(address->ifa_addr, family_struct_size, text_address, sizeof(text_address), 0, 0, NI_NUMERICHOST);
+    printf("    host:\t%s\n", text_address);
+    
+    getnameinfo(address->ifa_netmask, family_struct_size, text_address, sizeof(text_address), 0, 0, NI_NUMERICHOST);
+    printf("    netmask:\t%s\n", text_address);
+}
+
+void print_inet_interfaces(struct ifaddrs* addresses)
+{
+    uint8_t index = 1;
+    for(struct ifaddrs* address = addresses; address != NULL; address = address->ifa_next)
+    {
+        int addr_family = address->ifa_addr->sa_family;
+        if (addr_family != AF_INET && addr_family != AF_INET6)
+            continue;
+            
+        print_ifaddr_info(address, index);
+        ++index;
+    }
+}
+
 int main(int argc, char** argv)
 {
     struct ifaddrs* addresses;
@@ -14,24 +46,6 @@ int main(int argc, char** argv)
         return 1;
     }
     
-    uint8_t i = 1;
     printf("IPv4 and IPv6 interfaces:\n\n");
-    for(struct ifaddrs* address = addresses; address != NULL; address = address->ifa_next)
-    {
-        int addr_family = address->ifa_addr->sa_family;
-        if (addr_family != AF_INET && addr_family != AF_INET6)
-            continue;
-            
-        printf("Interface #%u:\n", i);
-        printf("    family:\t%s\n", (addr_family == AF_INET) ? "IPv4" : "IPv6");
-        printf("    name:\t%s\n", address->ifa_name);
-        
-        char text_address[100];
-        const int family_struct_size = (addr_family == AF_INET) ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
-        getnameinfo(address->ifa_addr, family_struct_size, text_address, sizeof(text_address), 0, 0, NI_NUMERICHOST);
-        printf("    host:\t%s\n", text_address);
-        getnameinfo(address->ifa_netmask, family_struct_size, text_address, sizeof(text_address), 0, 0, NI_NUMERICHOST);
-        printf("    netmask:\t%s\n", text_address);
-        ++i;
-    }
+    print_inet_interfaces(addresses);
 }
