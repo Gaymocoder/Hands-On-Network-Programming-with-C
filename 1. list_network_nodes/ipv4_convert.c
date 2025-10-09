e08551c0f2b5b053a01017e519e71e048cd475e5
Gaymocoder <gaymocoder@gmail.com>
2025-10-09 22:13:01 +0800

    B&W | 1LNN | Added "ipv4_convert"-module
    — Added ipv4_convert-module;
    — Added declaration of ipv4_convert_str_to_bytes() to "ipv4_convert"-module;
    — Added definition for the function to "ipv4_convert"-module;
    — Modified CMake-tree for new module;
    
    — Builds and works properly.

---
+++ 1. list_network_nodes/ipv4_convert.c
@@ -0,0 +1,35 @@
+#include "ipv4_convert.h"
+
+#include <math.h>
+#include <stdint.h>
+
+int ipv4_convert_str_to_bytes(char* str, int len, char* bytes)
+{
+    char buf = 0;
+    uint8_t byte_index = 0;
+    for(int i = 0; i <= len; ++i)
+    {
+        if (str[i] != '.' && i != len)
+            continue;
+        
+        if (byte_index > 3)
+            return -1;
+            
+        int j = i;
+        while (j-1 != -1 && str[j-1] != '.')
+        {
+            uint16_t new_buf = (str[j-1] - '0') * pow(10, i-j);
+            if (new_buf > 255)
+                return -1;
+                
+            buf = (char) new_buf;
+            --j;
+        }
+        
+        bytes[byte_index] = buf;
+        ++byte_index;
+        buf = 0;
+    }
+    
+    return 0;
+}
---
+++ 1. list_network_nodes/ipv4_convert.h
@@ -0,0 +1,8 @@
+#ifndef __1_LNN_IPV4_CONVERT_H__
+#define __1_LNN_IPV4_CONVERT_H__
+
+#include <ifaddrs.h>
+
+int ipv4_convert_str_to_bytes(char* str, int len, char* bytes);
+
+#endif
