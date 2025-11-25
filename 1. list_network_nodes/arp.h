c3aebf7b86b4d7cca8ab2558beca52dacbbc98f0
Gaymocoder <gaymocoder@gmail.com>
2025-11-26 01:11:24 +0800

    B&W | 1LNN | Added comments for ARP-struct
    — Added comments for ARP-packet-structure;
    
    — Builds and works properly.

--- 1. list_network_nodes/arp.h
+++ 1. list_network_nodes/arp.h
@@ -9,19 +9,19 @@
 #pragma pack(push, 1)
 struct ARP_Packet
 {
-    struct ether_header ethernet_header;
+    struct ether_header ethernet_header;    // Destination MAC + Source MAC + Protocol Type (ARP)
     
-    uint16_t hw_type;
-    uint16_t prtcl_type;
+    uint16_t hw_type;                       // Ethernet — 0x0001
+    uint16_t prtcl_type;                    // ARP      — 0x0800
     
-    uint8_t hw_length;
-    uint8_t prtcl_length;
-    uint16_t operation;
+    uint8_t hw_length;                      // For MAC  — 0x06
+    uint8_t prtcl_length;                   // For IPv4 — 0x04
+    uint16_t operation;                     // Request  — 0x0001
     
-    uint8_t sender_hw_addr[6];
-    uint8_t sender_prtcl_addr[4];
-    uint8_t target_hw_addr[6];
-    uint8_t target_prtcl_addr[4];
+    uint8_t sender_hw_addr[6];              // Source MAC (duplicate from ethernet_header)
+    uint8_t sender_prtcl_addr[4];           // Source IP
+    uint8_t target_hw_addr[6];              // Destination MAC (duplicate from ethernet_header)
+    uint8_t target_prtcl_addr[4];           // Destination IP
 };
 #pragma pack(pop)
 
